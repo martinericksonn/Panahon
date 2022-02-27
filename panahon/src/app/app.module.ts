c55458079ec8c0bdb997635cfdba0faef3e41158
Martin <lapetajemartin@gmail.com>
2022-02-27 20:18:06 +0800

    search page

--- panahon/src/app/app.module.ts
+++ panahon/src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { NgModule } from '@angular/core';
+import { AfterViewInit, ElementRef, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SearchComponent, WeatherComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
